1e23c5d33e2b9f9df85a465c2b9eb9b29ea2aa07
Madhukar Tattikota <madhukar.tattikota@inspirebrands.com>
2024-10-07 16:45:12 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { useState } from "react";
+import { Carousel } from "react-responsive-3d-carousel";
+
+export default function Home() {
+  const [width, setWidth] = useState("700px");
+  const [height, setHeight] = useState("500px");
+
+  return (
+    <div>
+      <section>
+        <div id="hero" className="h-screen relative"></div>
+      </section>
+      <section>
+        <div className="relative max-w-6xl flex flex-col mx-auto  justify-between px-4 my-32 ">
+          <div className="flex flex-col space-y-2 justify-between md:flex-row">
+            <div className="flex flex-col items-start space-y-8 w-72">
+              <div className="font-bold text-2xl">
+                <strong>Battery</strong>
+              </div>
+              <div className="">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of
+              </div>
+            </div>
+            <div className="flex flex-col items-start space-y-8 w-72 font-bold  md:flex-col-reverse">
+              <div className="font-bold text-2xl">
+                <strong>1 year Warranty</strong>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img
+              className="block w-full bg-cover bg-center"
+              src="/image351.png"
+              alt=""
+            />
+          </div>
+          <div className="flex flex-row-reverse">
+            <div className="flex flex-col items-center justify-between space-y-2 md:text-right">
+              <div className="w-64 text-4xl font-bold text-lime-400">
+                <strong className="w-64 text-4xl font-bold text-lime-400">
+                  333 &nbsp;
+                </strong>
+                <strong className="w-64 text-2xl font-bold text-lime-400">
+                  KM
+                </strong>
+              </div>
+              <div className="w-64 text-4xl font-normal">
+                <strong>RANGE</strong>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="relative container flex flex-col max-w-screen-xl mx-auto space-y-2 justify-between px-4 my-32 md:flex-row md:space-x-2 md:space-y-0">
+          <div>
+            <img
+              className="block w-full bg-cover bg-center"
+              src="/222.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              className="block w-full bg-cover bg-center"
+              src="/7778.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="relative max-w-screen-xl flex flex-col mx-auto items-center justify-between px-4 my-32 md:flex-row">
+          <Carousel
+            showArrows={false}
+            width={width}
+            height={height}
+            autoPlay={true}
+            spread="wide"
+            showStatus={false}
+            showIndicators={false}
+          >
+            <img src="image340.png" alt="example-image-1" />
+            <img src="image-1.png" alt="example-image-2" />
+            <img src="image340.png" alt="example-image-2" />
+
+            <img src="image.png" alt="example-image-1" />
+            <img src="image-1.png" alt="example-image-2" />
+          </Carousel>
+        </div>
+      </section>
+      <section>
+        <div className="relative max-w-screen-xl flex flex-col mx-auto items-center justify-between px-4 my-32 md:flex-row">
+          <img
+            className="block w-full bg-cover bg-center"
+            src="/image342.png"
+            alt=""
+          />
+        </div>
+      </section>
+
+      <section>
+        <div className="relative max-w-screen-xl flex flex-col mx-auto items-center justify-between px-4 my-32 md:flex-row">
+          <img
+            className="block w-full bg-cover bg-center"
+            src="/image346.png"
+            alt=""
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
---
+++ src/app/product/page.tsx
@@ -0,0 +1,214 @@
+"use client";
+
+export default function Product() {
+  return (
+    <div>
+      <section>
+        <div id="hero-product" className="h-screen relative"></div>
+      </section>
+      <section>
+        <div className="container max-w-screen-xl mx-auto my-32 px-4 md:px-0">
+          <div className="flex flex-col items-center py-10">
+            <strong className="text-3xl">Highlights</strong>
+          </div>
+          <div className="columns-1 sm-columns-2 md:columns-4 md:gap-0">
+            <div className="break-inside-avoid">
+              <img
+                className="w-full object-cover"
+                src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05459_ziuomy.jpg"
+                alt=""
+              />
+            </div>
+            <div className="break-inside-avoid">
+              <img
+                className="w-full object-cover"
+                src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05626_ytsf3j.jpg"
+                alt=""
+              />
+            </div>
+            <div className="break-inside-avoid">
+              <img
+                className="w-full object-cover"
+                src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814784/photostream-photos/DSC05501_yirmq8.jpg"
+                alt=""
+              />
+            </div>
+            <div className="break-inside-avoid">
+              <img
+                className="w-full object-cover"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+                alt=""
+              />
+            </div>
+            <div className="break-inside-avoid">
+              <img
+                className="w-full object-cover"
+                src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05449_l9kukz.jpg"
+                alt=""
+              />
+            </div>
+            <div className="break-inside-avoid">
+              <img
+                className="w-full object-cover"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+                alt=""
+              />
+            </div>
+            <div className="break-inside-avoid">
+              <img
+                className="w-full object-cover"
+                src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05513_gfbiwi.jpg"
+                alt=""
+              />
+            </div>
+            <div className="break-inside-avoid">
+              <img
+                className="w-full object-cover"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+                alt=""
+              />
+            </div>
+            <div className="break-inside-avoid">
+              <img
+                className="w-full object-cover"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+                alt=""
+              />
+            </div>
+            <div className="break-inside-avoid">
+              <img
+                className="w-full object-cover"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+                alt=""
+              />
+            </div>
+            <div className="break-inside-avoid">
+              <img
+                className="w-full object-cover"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container max-w-screen-xl mx-auto my-32 px-4 md:px-0">
+          <div className="grid md:grid-cols-3">
+            <div>
+              <img
+                className="h-auto max-w-full"
+                src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05466_kwlv0n.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full "
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full "
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="h-auto max-w-full"
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container max-w-screen-xl mx-auto my-32 px-4 md:px-0">
+          <div className="flex flex-col items-center space-y-2 md:flex-row md:items-start md:space-x-8 md:space-y-0">
+            <div className="flex flex-col space-y-4 md:w-2/5">
+              <div className="text-3xl">
+                <strong>Cluster</strong>
+              </div>
+              <div>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of
+              </div>
+            </div>
+            <div className="flex-2 md:w-3/5">
+              <div>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
